d0f973cf4bd129984673e7fefcca72eb49d97cc5
Sagar Parmar <sagarparmar881@gmail.com>
2025-09-18 09:45:57 +0530

    feat :: api-gateway :: Added routes for Product, Order and Inventory service.

--- src/main/java/com/sagar/microservices/routes/Routes.java
+++ src/main/java/com/sagar/microservices/routes/Routes.java
@@ -10,10 +10,25 @@ import org.springframework.web.servlet.function.*;
 @Configuration
 public class Routes {
 
-    @Bean
-    public RouterFunction<ServerResponse> routes() {
+    @Bean("productServiceRouterFunction")
+    public RouterFunction<ServerResponse> productServiceRoute() {
         return GatewayRouterFunctions.route("product_service")
                 .route(RequestPredicates.path("api/product"), HandlerFunctions.http("http://localhost:8080/"))
                 .build();
     }
+
+    @Bean("orderServiceRouterFunction")
+    public RouterFunction<ServerResponse> orderServiceRoute() {
+        return GatewayRouterFunctions.route("order_service")
+                .route(RequestPredicates.path("api/order"), HandlerFunctions.http("http://localhost:8081/"))
+                .build();
+    }
+
+    @Bean("inventoryServiceRouterFunction")
+    public RouterFunction<ServerResponse> inventoryServiceRoute() {
+        return GatewayRouterFunctions.route("inventory_service")
+                .route(RequestPredicates.path("api/inventory"), HandlerFunctions.http("http://localhost:8082/"))
+                .build();
+    }
+
 }
